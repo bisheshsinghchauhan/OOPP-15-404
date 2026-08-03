8af2c212cbbf9e957170be30e67f416e3a10dc7e
Bishesh Singh Chauhan <bisheshsinghchauhan@gmail.com>
2026-08-03 11:46:03 +0530

    commit

--- prg2.cpp
+++ prg2.cpp
@@ -1,41 +1,39 @@
-// Develop a program to demonstrate parameter passing techniques (call by value, reference, and
-// address) and analyze their effects on variable modification. performing swapping.
-#include <iostream>
-using namespace std;
-void swapByvalue(int a,int b) {
-    int temp;
-    temp=a;
-    a=b;
-    b=temp;
-}
-void swapByreference(int &x , int &y) {
-    int temp;
-    temp=x;
-    x=y;
-    y=temp;
-    
-}
-void swapByaddress(int *c , int *d) {
-    int temp;
-    temp=*c;
-    *c=*d;
-    *d=temp;
-}
+// #include <iostream>
+// using namespace std;
+// void swapByvalue(int a,int b) {
+//     int temp;
+//     temp=a;
+//     a=b;
+//     b=temp;
+// }
+// void swapByreference(int &x , int &y) {
+//     int temp;
+//     temp=x;
+//     x=y;
+//     y=temp;
+// }
+// void swapByaddress(int *c , int *d) {
+//     int temp;
+//     temp=*c;
+//     *c=*d;
+//     *d=temp;
+// }
+// int main() {
+//     int e = 10;
+//     int f = 20;
+//     cout<<e<<" "<<f<<endl;
+//     swapByvalue(e,f);
+//     cout<<"new = "<<e<<" "<<f<<endl;
+//     int x = 10;
+//     int y = 20;
+//     cout<<x<<" "<<y<<endl;
+//     swapByreference(x, y);
+//     cout<<"new = "<<x<<" "<<y<<endl;
+//     int c = 10;
+//     int d = 20;
+//     cout<<c<<" "<<d<<endl;
+//     swapByaddress(&c, &d);
+//     cout<<"new = "<<c<<" "<<d<<endl;
+//     return 0;
+// }
 
-int main() {
-    int e = 20;
-    int f = 10;
-    swapByvalue(e,f);
-    cout<<e<<" "<<f<<endl;
-    int x = 10;
-    int y = 20;
-    swapByreference(x, y);
-    cout << x << " ";
-    cout << y << endl;
-    int c = 10;
-    int d = 20;
-    swapByaddress(&c, &d);
-    cout << c << " ";
-    cout << d << endl;
-    return 0;
-}
