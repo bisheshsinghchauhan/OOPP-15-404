e2ea1a5de34467b8a97b7cc68a415a245b3f2c3a
Bishesh Singh Chauhan <bisheshsinghchauhan@gmail.com>
2026-08-31 11:52:43 +0530

    prg9

--- prg9.cpp
+++ prg9.cpp
@@ -28,7 +28,6 @@ int main() {
         cout << "\tStudent " << i + 1 << endl;
         (pointer + i)->input();
     }
-    
     cout << "\nRoll\tName\tMarks\n";
     for (int i = 0; i < n; i++) {
         (pointer + i)->display();
